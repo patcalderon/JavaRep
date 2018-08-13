3597575037e16a6cb30410e1d994e98f8d2bd297
Patricio Calderon <patricio.calderon@outlook.com>
2018-08-13 13:59:35 +0200

    Rest. uppgifter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,67 @@ import java.util.Scanner;
 
 public class Main {
 
-    for(int i=1;i<100;i++){
-        if(i%100==0) {
+    public static void main(String[] args) {
+// Kokar vattnet?
+    int vattentemp =100;
+
+        if (vattentemp == 100) {
             System.out.println("Vattnet kokar");
-        }
-        else if (i%50==0) {
+        } else if (vattentemp == 50) {
             System.out.println("Det är halvvägs nu!");
-        }
-        else if (i%==0) {
+        } else if (vattentemp == 0){
             System.out.println("Vattnet kokar inte");
         }
-        else {
-            System.out.println(i);
+    }
+// Går det att bada?
+        private int vatten;
+
+        public boolean vatten() {
+            if ((vatten) < 30) {
+                return true;
+                System.out.println("Varsågod och bada!");
+            }
+            else {(vatten) > 30) {
+                return false;
+                System.out.println("Det går inte att bada!");
+            }
         }
+    }
+// Vad blir det för middag?
+    public class dagar {
+            public void main () {
+                int dag;
+                dag = 0;
+                switch dag() {
+                    Måndag:
+                    System.out.println("Kyckling");
+                    Tisdag:
+                    System.out.println("Pankaka");
+                    Onsdag:
+                    System.out.println("Ärtsoppa");
+                    Torsdag:
+                    System.out.println("Gryta");
+                    Fredag;
+                    System.out.println("Kalv");
+                    Lördag;
+                    System.out.println("Vegetariskt");
+                    Söndag;
+                    System.out.println("Kålsoppa");
+                }
+            }
+    }
+// For loop
+    public class foorlop {
+
+        public void main (){
+
+            int a;
+            int b = 10;
+
+            for (a = 0; a < b; a++);
+
+                System.out.println("Värde =" +  a);
+
+        }
+    }
 }

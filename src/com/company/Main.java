30efbe50559a5a9b4fdb3f79af180b7bbdf00762
Patricio Calderon <patricio.calderon@outlook.com>
2018-08-13 10:56:48 +0200

    Add src folder with code

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    for(int i=1;i<100;i++){
+        if(i%100==0) {
+            System.out.println("Vattnet kokar");
+        }
+        else if (i%50==0) {
+            System.out.println("Det är halvvägs nu!");
+        }
+        else if (i%==0) {
+            System.out.println("Vattnet kokar inte");
+        }
+        else {
+            System.out.println(i);
+        }
+}
